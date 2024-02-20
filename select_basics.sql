ad6670baa28c34c1dca1f5e969aa2abcee8570d9
Fenyx-Forte <fenyxforte@gmail.com>
2024-02-20 20:23:12 -0300

    reescrevendo as queries de maneira mais legivel

--- select_basics.sql
+++ select_basics.sql
@@ -2,13 +2,41 @@
 SELECT population FROM world
 WHERE name = 'Germany';
 
+-- New
+select
+  w.population
+from
+  world as w
+where
+  w.name = "Germany";
+
 -- Task 2
 SELECT name, population FROM world
 WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
+-- New
+select
+    w.name
+  , w.population
+from
+  world as w
+where
+  w.name IN ("Sweden"
+            , "Norway"
+            , "Denmark");
+
 -- Task 3
 SELECT name, area FROM world
 WHERE area BETWEEN 200000 AND 250000;
+
+-- New
+select
+    w.name
+  , w.area
+from
+  world as w
+where
+  w.area between 200000 and 250000;
 
 
 -- SELECT Quiz
--- select_from_nobel.sql
+++ select_from_nobel.sql
@@ -3,6 +3,16 @@ SELECT yr, subject, winner
 FROM nobel
 WHERE yr = 1950;
 
+-- New
+select
+    n.yr
+    , n.subject
+    , n.winner
+from
+    nobel as n
+where
+    n.yr = 1950;
+
 
 -- Task 2
 SELECT winner
@@ -10,11 +20,28 @@ FROM nobel
 WHERE yr = 1962
     AND subject = 'literature';
 
+-- New
+select
+    n.winner
+from
+    nobel as n
+where
+    n.yr = 1962
+    and n.subject = "literature";
 
 -- Task 3
 SELECT yr, subject
 FROM nobel
 WHERE winner = "Albert Einstein";
+
+-- New
+select
+    n.yr
+    , n.subject
+from
+    nobel as n
+where
+    n.winner = "Albert Einstein";
 
 -- Task 4
 SELECT winner
@@ -22,11 +49,31 @@ FROM nobel
 WHERE yr >= 2000
     AND subject = "peace";
 
+-- New
+select
+    n.winner
+from
+    nobel as n
+where
+    n.yr >= 2000
+    and n.subject = "peace";
+
 -- Task 5
 SELECT yr, subject, winner
 FROM nobel
 WHERE yr BETWEEN 1980 AND 1989
     AND subject = "literature";
+
+-- New
+select
+    n.yr
+    , n.subject
+    , n.winner
+from
+    nobel as n
+where
+    n.yr between 1980 and 1989
+    and n.subject = "literature";
 
 -- Task 6
 SELECT * 
@@ -34,12 +81,29 @@ FROM nobel
 WHERE winner IN ("Theodore Roosevelt", "Thomas Woodrow Wilson", 
                 "Jimmy Carter", "Barack Obama");
 
+-- New
+select
+    *
+from
+    nobel as n
+where
+    n.winner in ("Theodore Roosevelt"
+                , "Thomas Woodrow Wilson" 
+                , "Jimmy Carter"
+                , "Barack Obama");
 
 -- Task 7
 SELECT winner
 FROM nobel
 WHERE LEFT(winner, 5) = "John ";
 
+-- New
+select
+    n.winner
+from
+    nobel as n
+where
+    left(n.winner, 5) = "John";
 
 -- Task 8
 SELECT yr, subject, winner
@@ -47,6 +111,19 @@ FROM nobel
 WHERE (subject = "physics" AND yr = 1980)
     OR (subject = "chemistry" AND yr = 1984);
 
+-- New
+select
+    n.yr
+    , n.subject
+    , n.winner
+from
+    nobel as n
+where
+    (n.subject = "physics"
+    and n.yr = 1980)
+    or
+    (n.subject = "chemystry"
+    and n.yr = 1984);
 
 -- Task 9
 SELECT yr, subject, winner
@@ -54,6 +131,17 @@ FROM nobel
 WHERE yr = 1980
     AND subject NOT IN ("chemistry", "medicine");
 
+-- New
+select
+    n.yr
+    , n.subject
+    , n.winner
+from
+    nobel as n
+where
+    n.yr = 1980
+    and n.subject not in ("chemistry"
+                        , "medicine");
 
 -- Task 10
 SELECT yr, subject, winner
@@ -61,15 +149,49 @@ FROM nobel
 WHERE (subject = "medicine" AND yr < 1910)
     OR (subject = "literature" AND yr >= 2004);
 
+-- New
+select
+    n.yr
+    , n.subject
+    , n.winner
+from
+    nobel as n
+where
+    (n.subject = "medicine"
+    and n.yr < 1910)
+    or
+    (n.subject = "literature"
+    and n.yr >= 2004);
+
 -- Task 11
 SELECT yr, subject, winner
 FROM nobel
 WHERE winner = "PETER GRÜNBERG";
 
+-- New
+select
+    n.yr
+    , n.subject
+    , n.winner
+from
+    nobel as n
+where
+    n.winner = "PETER GRÜNBERG";
+
 -- Task 12
 SELECT yr, subject, winner
 FROM nobel
 WHERE winner = "EUGENE O'NEILL";
+
+-- New
+select
+    n.yr
+    , n.subject
+    , n.winner
+from
+    nobel as n
+where
+    n.winner = "EUGENE O'NEILL";
 
 -- Task 13
 SELECT winner, yr, subject
@@ -77,11 +199,37 @@ FROM nobel
 WHERE LEFT(winner, 4) = "Sir " 
 ORDER BY yr DESC, winner;
 
+-- New
+select
+    n.winner
+    , n.yr
+    , n.subject
+from
+    nobel as n
+where
+    left(n.winner, 4) = "Sir "
+order by
+    n.yr desc
+    , n.winner asc;
+
 -- Task 14
 SELECT winner, subject
 FROM nobel
 WHERE yr = 1984
 ORDER BY (subject IN ('physics','chemistry')), subject, winner;
+
+-- New
+select
+    n.winner
+    , n.subject
+from
+    nobel as n
+where
+    n.yr = 1984
+order by
+    (n.subject in ("physics", "chemistry")) asc
+    , n.subject asc
+    , n.winner asc
 
 -- Nobel Quiz
 -- Q1) e
